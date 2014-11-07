08b847a8dbee32ed8b5e79cac2e01dd36ff65d70
rovepe17 <rociounipamplona@hotmail.com>
2014-11-07 18:38:33 -0500

    Appcalculadora

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Appcalculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numero1 = new System.Windows.Forms.Label();
+            this.numero2 = new System.Windows.Forms.Label();
+            this.resultado = new System.Windows.Forms.Label();
+            this.suma = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // numero1
+            // 
+            this.numero1.AutoSize = true;
+            this.numero1.Location = new System.Drawing.Point(42, 59);
+            this.numero1.Name = "numero1";
+            this.numero1.Size = new System.Drawing.Size(50, 13);
+            this.numero1.TabIndex = 0;
+            this.numero1.Text = "Numero1";
+            this.numero1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // numero2
+            // 
+            this.numero2.AutoSize = true;
+            this.numero2.Location = new System.Drawing.Point(42, 97);
+            this.numero2.Name = "numero2";
+            this.numero2.Size = new System.Drawing.Size(50, 13);
+            this.numero2.TabIndex = 1;
+            this.numero2.Text = "Numero2";
+            // 
+            // resultado
+            // 
+            this.resultado.AutoSize = true;
+            this.resultado.Location = new System.Drawing.Point(42, 134);
+            this.resultado.Name = "resultado";
+            this.resultado.Size = new System.Drawing.Size(55, 13);
+            this.resultado.TabIndex = 2;
+            this.resultado.Text = "Resultado";
+            // 
+            // suma
+            // 
+            this.suma.Location = new System.Drawing.Point(114, 177);
+            this.suma.Name = "suma";
+            this.suma.Size = new System.Drawing.Size(75, 23);
+            this.suma.TabIndex = 3;
+            this.suma.Text = "Suma";
+            this.suma.UseVisualStyleBackColor = true;
+            this.suma.Click += new System.EventHandler(this.suma_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(98, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(98, 97);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(98, 131);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.suma);
+            this.Controls.Add(this.resultado);
+            this.Controls.Add(this.numero2);
+            this.Controls.Add(this.numero1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label numero1;
+        private System.Windows.Forms.Label numero2;
+        private System.Windows.Forms.Label resultado;
+        private System.Windows.Forms.Button suma;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Appcalculadora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void suma_Click(object sender, EventArgs e)
+        {
+            int numero1, numero2, suma;
+            numero1 = int.Parse(txtNumero1.Text);
+            numero2 = int.Parse(txtNumero2.Text);
+            suma = numero1 + numero2;
+            lblResultado.Txt = "Resultado: " + suma.ToString();
+
+
+        }
+    }
+}
